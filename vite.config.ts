c6ed9bc2293482def463fd092951ae4a9f4b3018
Bryan <git@singee.me>
2025-11-10 10:29:17 +0800

    use CSS modules, remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
 import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
@@ -10,7 +9,6 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tailwindcss(),
     tanstackStart(),
     nitro({ config: { preset: 'node-server' } }),
     viteReact(),
