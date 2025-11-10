90a944a84a9d87a505f29dc063a7498ab29410ac
Bryan <git@singee.me>
2025-11-10 10:29:46 +0800

    use node-server for tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro({ config: { preset: 'node-server' } }),
     viteReact(),
   ],
 });
