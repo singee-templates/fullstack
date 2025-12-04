205fc88928333e7921d8efb63783f0b0b2ff495b
Bryan <git@singee.me>
2025-12-04 13:38:29 +0800

    upgrade dependencies

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,16 +1,10 @@
 import { betterAuth } from 'better-auth/minimal';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { tanstackStartCookies } from 'better-auth/tanstack-start';
 
 // FIXME: We have to use ../db (rather than ~db) here to make @better-auth/cli happy
 // See https://github.com/better-auth/better-auth/issues/6373
 import { db } from '../db';
-
-// FIXME: We should use import { tanstackStartCookies } from 'better-auth/tanstack-start'
-// But it's impossible to do so due to a bug in tanstack-start
-// See:
-//  - https://github.com/better-auth/better-auth/issues/6341
-//  - https://github.com/TanStack/router/issues/5795
-import { tanstackStartCookies } from './tanstack-start';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
